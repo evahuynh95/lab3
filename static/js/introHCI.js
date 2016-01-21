e0c03ad31e3304a6af67f901d0c0758b7d51b6ba
Eva Huynh <e4huynh@ucsd.edu>
2016-01-21 20:09:52 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,43 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Button is Pressed");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(submitClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+function projectClick(e){
+	console.log("Project clicked");
+	e.preventDefault();
+	var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project");
+    $(containingProject).css("background-color", "#00FFFF");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.toggle();
+    }
+
+    //$(containingProject).css("background-color", "#000000");
+}
+
+function submitClick(e){
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
+
